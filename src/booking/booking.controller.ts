bd5b1f554ca89d3cc8152c37788f9611352ca24a
Priyadharshini V <priyadharshinimohan04@gmail.com>
2026-02-23 20:49:35 +0530

    bookings

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Post, UseGuards, Req, Patch } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Req, Patch,Body, } from '@nestjs/common';
 import { BookingsService } from './booking.service';
 import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import type { Request } from 'express';
 import { BookingStatus } from '../entities/booking.entity';
 
 @Controller('bookings')
@@ -10,7 +10,24 @@ export class BookingController {
     constructor(private readonly bookingService: BookingsService) { }
 
     @Post()
-    createBooking() { }
+  createBooking(
+    @Body()
+    body: {
+      bikeId: string;
+      startDate: Date;
+      endDate: Date;
+    },
+    @Req() req: Request,
+  ) {
+    const user = (req as any).user;
+
+    return this.bookingService.createBooking(
+      body.bikeId,
+      user.userId,
+      body.startDate,
+      body.endDate,
+    );
+  }
 
     @Get()
     getAllBookings() {
